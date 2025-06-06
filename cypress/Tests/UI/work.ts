79632a3ec79c7a42a7220a2533b4c6f2297692b9
VahagnYeghoyan <vahagn.yeghoyan@arloopa.com>
2025-06-06 15:45:23 +0400

    cypress commands

--- cypress/Tests/UI/work.ts
+++ cypress/Tests/UI/work.ts
@@ -1,10 +1,20 @@
 describe('Actions on Dom Elements', () => {
        context('Input', () =>{
+        beforeEach(() =>{
+         cy.visit("your-pge-url")
+        });
         it('will check the input data', () => {
-         cy.get('input')
+         cy.get('email').type("test@gmail.com");
+         cy.get("email").should('have.test@gmail.com');
        })
        context('Checkbox', () =>{
-
+        it('will check the Checkbox function', ()=>{
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').check()
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').should('be.checked')
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').uncheck()
+        cy.get('.action-checkboxes [type="checkbox"]').not('[disabled]').should('not.be.checked')
+       });
+      
        })
        context('Paragraph', () =>{
 
