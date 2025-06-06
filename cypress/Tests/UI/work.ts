c9ecf19a84c586245cb1494ee03b0a627aee5ea1
VahagnYeghoyan <vahagn.yeghoyan@arloopa.com>
2025-06-06 14:11:18 +0400

    testing

---
+++ cypress/Tests/UI/work.ts
@@ -0,0 +1,13 @@
+describe('Actions on Dom Elements', () => {
+       context('Input', () =>{
+        it('will check the input data', () => {
+         cy.get('input')
+       })
+       context('Checkbox', () =>{
+
+       })
+       context('Paragraph', () =>{
+
+       })
+       
+  })
