b4429519d3b957abb01c21dc0bca9ac372cac20b
arthuratayan <arthuratayan@gmail.com>
2025-06-07 20:27:05 +0400

    homework-dashboard
    homework for selectors

---
+++ Cypress/Tests/UI/arthur/homework-dashboard.ts
@@ -0,0 +1,34 @@
+describe("Test scenarios for Admin Dashboard", () => {
+  beforeEach(() => {
+    cy.visit("http://192.168.1.43:8080/Resources/htmls/CSS/homework.html");
+  });
+
+  it("Should fill in and submit the user form with valid data", () => {
+    cy.get("#username").should("be.visible").type("testuser").should("have.value", "testuser");
+
+    cy.get("#email").type("testuser@example.com").should("have.value", "testuser@example.com");
+
+    cy.get("#role").select("Editor").should("have.value", "editor");
+
+    cy.get("#newsletter").check().should("be.checked");
+
+    cy.get('button[type="submit"]').contains("Submit").click();
+
+    cy.get("#username").should("have.value", "");
+    cy.get("#email").should("have.value", "");
+    cy.get("#role").should("have.value", "");
+    cy.get("#newsletter").should("not.be.checked");
+  });
+
+  it("Should check User table and edit data", () => {
+    cy.get("table.user-table tbody tr").should("have.length", 3);
+    cy.get("table.user-table tbody tr").eq(0).find("button").contains("Edit").click();
+    cy.get("table.user-table tbody tr").eq(1).find("button").contains("Edit").click();
+    cy.get("table.user-table tbody tr").eq(2).find("button").should("be.disabled");
+  });
+
+  it("Should check navigation and footer", () => {
+    cy.get("aside.sidebar ul li a").should("have.length", 3);
+    cy.get(".footer").should("contain", "© 2025 TestCorp");
+  });
+});
