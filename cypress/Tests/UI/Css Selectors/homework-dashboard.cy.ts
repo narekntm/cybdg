cabc7d52916945bff0a2ba7ea5274b39b82e5441
VahagnYeghoyan <vahagn.yeghoyan@arloopa.com>
2025-06-13 15:02:59 +0400

    homework

--- cypress/Tests/UI/Css Selectors/homework-dashboard.cy.ts
+++ cypress/Tests/UI/Css Selectors/homework-dashboard.cy.ts
@@ -1,8 +1,18 @@
 describe('Admin Dashboard', () => {
   beforeEach(() =>{
-    cy.visit (http://192.168.88.204:8080/Resources/htmls/CSS/homework.html
+    cy.visit (file:///Users/vahagnyeghoyan/Desktop/Project/cybdg/Resources/htmls/CSS/homework.html
         )
-  it('should validates editor login flow', () => {
-    cy.get
+  it('should be filled and submited correctly', () => {
+    cy.get('input[name='Username']').type('testuser');
+    cy.get('input[name='Email']').type('testuser@example.com');
+    cy.get('select[name='role']').select('editor');
+    cy.get('input[name='Subscribe to newsletter']').check();
+    cy.get('button[type='Submit']').click();
+  });
+  it('should validate row actions and button states', () => {
+    cy.get('table tbody tr').should('have.length', 3);
+    cy.get('table tbody tr').eq(0).find('button.edit-btn').click();
+    cy.get('table tbody tr').eq(1).find('button.edit-btn').click();
+    cy.get('table tbody tr').eq(2).find('button.edit-btn').should('be.disabled');
   });
 })
