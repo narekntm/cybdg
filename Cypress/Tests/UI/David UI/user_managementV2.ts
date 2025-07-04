fab1fd6f555feb4cdff4b03e83cb516c4ed3a1bc
dav-003 <dav.hovhannisyan52@gmail.com>
2025-07-04 13:02:08 +0400

    Generators

--- Cypress/Tests/UI/David UI/user_managementV2.ts
+++ Cypress/Tests/UI/David UI/user_managementV2.ts
@@ -1,5 +1,5 @@
 import { UserManagementPage } from "Pages/UserManagementPage";
-import { UserManagementMethods } from "Pages/David Methods/UserManagementMethods";
+import { UserManagementMethods } from "../../../Fixtures/David Methods/UserManagementMethods";
 import { SignIn } from "Models/David Models/UserManagementModels";
 import { UserManagementModels } from "Models/Lecture/UserManagementModels";
 import Chance from "chance";
