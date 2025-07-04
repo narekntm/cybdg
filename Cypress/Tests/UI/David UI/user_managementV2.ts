452755c0a084284164c400e441bb67c308b22418
dav-003 <dav.hovhannisyan52@gmail.com>
2025-07-04 13:10:55 +0400

    changes

--- Cypress/Tests/UI/David UI/user_managementV2.ts
+++ Cypress/Tests/UI/David UI/user_managementV2.ts
@@ -1,7 +1,7 @@
 import { UserManagementPage } from "Pages/UserManagementPage";
 import { UserManagementMethods } from "../../../Fixtures/David Methods/UserManagementMethods";
-import { SignIn } from "Models/David Models/UserManagementModels";
 import { UserManagementModels } from "Models/Lecture/UserManagementModels";
+import {UserManagementGenerators} from "Generators/David Generators/UserManagementGenerators";
 import Chance from "chance";
 import { UserManagementBuilders } from "Builders/David Builders/UserManagementBuilders";
 const chance = new Chance();
@@ -11,8 +11,6 @@ describe('Auth', () => {
         UserManagementBuilders.ResetData()
         cy.visit('/')
     })
-    const login = 'admin@example.com';
-    const password = 'admin123';
     
     context('Negative cases', () => {
         it('Empty fields', () => {
@@ -20,17 +18,17 @@ describe('Auth', () => {
             UserManagementPage.invalidCredentials().should('be.visible')
         })
         it('Wrong email', () => {
-            UserManagementMethods.AuthV2({password: password})
+            UserManagementMethods.AuthV2({password: UserManagementGenerators.adminCredentials.password})
             UserManagementPage.invalidCredentials().should('be.visible')
         })
         it('Wrong password', () => {
-            UserManagementMethods.AuthV2({email: login})
+            UserManagementMethods.AuthV2({email: UserManagementGenerators.adminCredentials.login})
             UserManagementPage.invalidCredentials().should('be.visible')
         })
     })
     context('Positive cases', () => {
         it('Should log in and delete admin', () => {
-            UserManagementMethods.AuthV2({email: login, password: password})
+            UserManagementMethods.AuthV2({email: UserManagementGenerators.adminCredentials.login, password: UserManagementGenerators.adminCredentials.password})
             UserManagementPage.AboutSiteButton().should('be.visible')
             UserManagementMethods.adminUserDeleteAsAdmin()
             UserManagementMethods.logoutV2()
@@ -42,46 +40,72 @@ describe('Add New user section', () => {
     beforeEach(() => {
         cy.visit('/')
     })
-    const name = "Joe";
-    const email = `${Date.now()}@example.com`;
-    const age = 18;
-    const role = "Admin";
-    const gender = "Male";
-    const subscribtion = "Product Updates";
-    const status = "Active";
-
 
     context('Positive cases', () => {
         const users: UserManagementModels.User[] = []
 
         for(let i = 0; i < 50; i++){
-            users.push({
-                name: chance.name().split(" ")[0],
-                role: chance.pickone(Object.values(UserManagementModels.Role)),
-                age: chance.integer({min:1, max:100}),
-                email: chance.email(),
-                gender: chance.pickone(Object.values(UserManagementModels.Gender)),
-                subscriptions: chance.pickset(Object.values((UserManagementModels.Subscription)),chance.integer({min:0, max:3}),)
-            })
+            const user = UserManagementGenerators.generatedUsers();
+            user.email = `user${i}@mails.com`;
+            users.push(user);
         }
         before(() => {
             UserManagementBuilders.ResetData()
             UserManagementBuilders.seedData(users)
         })
+        it('Should check pagination work', () => {
+            const defaultUserCount = 3
+            const seededUsersCount = users.length
+            const expectedUserCount = defaultUserCount + seededUsersCount;
+            const emails = [
+                ...users.map(user => user.email),
+                "alice@site.com",
+                "bob@site.com",
+                "eve@site.com",
+            ].filter(Boolean);
+            const seenEmails = new Set();
+
+            UserManagementPage.paginationInfo()
+              .should('be.visible')
+              .and('contain.text', `Page 1 of ${Math.ceil((expectedUserCount) / 5)}: (${expectedUserCount} Users)`);
+
+            UserManagementPage.prevPageButton().should('be.disabled')
+            UserManagementPage.nextPageButton().should('be.enabled')
+
+            function checkPage() {
+                cy.get("tbody tr").each(($tr) => {
+                    cy.wrap($tr).find("td").eq(3).invoke("text").then((text) => {
+                        seenEmails.add(text.trim());
+                    });
+                });
+
+                UserManagementPage.nextPageButton().then(($btn) => {
+                    if (!$btn.is(':disabled')) {
+                        cy.wrap($btn).click();
+                        checkPage()
+                    } else {
+                        cy.wrap(null).then(() => {
+                            expect(Array.from(seenEmails)).to.have.members(emails);
+                        });
+                    }
+                });
+            }
+            checkPage()
+        })
         it('Should check all positive cases with this section', () => {
             UserManagementPage.newUserButtonPopUpOpen().click()
-            UserManagementPage.nameField().type(name)
-            UserManagementPage.nameField().should('have.value', name)
+            UserManagementPage.nameField().type(UserManagementGenerators.validUser.name)
+            UserManagementPage.nameField().should('have.value', UserManagementGenerators.validUser.name)
             UserManagementPage.roleField().select('')
             UserManagementPage.roleOptionAdminV2().should('be.visible')
             UserManagementPage.roleOptionEditorV2().should('be.visible')
             UserManagementPage.roleOptionViewerV2().should('be.visible')
-            UserManagementPage.roleField().select(role)
-            UserManagementPage.roleField().should('have.value', role)
-            UserManagementPage.ageField().type(age.toString())
-            UserManagementPage.ageField().should('have.value', age)
-            UserManagementPage.emailField().type(email)
-            UserManagementPage.emailField().should('have.value', email)
+            UserManagementPage.roleField().select(UserManagementGenerators.validUser.role)
+            UserManagementPage.roleField().should('have.value', UserManagementGenerators.validUser.role)
+            UserManagementPage.ageField().type(String(UserManagementGenerators.validUser.age))
+            UserManagementPage.ageField().should('have.value', UserManagementGenerators.validUser.age)
+            UserManagementPage.emailField().type(UserManagementGenerators.validUser.email)
+            UserManagementPage.emailField().should('have.value', UserManagementGenerators.validUser.email)
             UserManagementPage.genderMaleRadioButton().should('not.be.checked').check()
             UserManagementPage.genderMaleRadioButton().should('be.checked')
             UserManagementPage.productUpdatesCheckbox().should('not.be.checked').check()
@@ -112,39 +136,6 @@ describe('Add New user section', () => {
             UserManagementPage.successPopUp().should('be.visible')
             UserManagementPage.userCardBackButton().click()
             UserManagementPage.userViewCardInfo().should('not.be.visible')
-        })
-        it('Should check pagination work', () => {
-            const emails = [...users.map(user => user.email),"alice@site.com","bob@site.com","eve@site.com",email].filter(Boolean)
-            const expectedUserCount = emails.length
-            const seenEmails = new Set();
-
-            UserManagementPage.paginationInfo()
-              .should('be.visible')
-              .and('contain.text', `Page 1 of ${Math.ceil((expectedUserCount) / 5)}: (${expectedUserCount} Users)`);
-
-            UserManagementPage.prevPageButton().should('be.disabled')
-            UserManagementPage.nextPageButton().should('be.enabled')
-
-            function checkPage() {
-                cy.get("tbody tr").each(($tr) => {
-                    cy.wrap($tr).find("td").eq(3).invoke("text").then((text) => {
-                        seenEmails.add(text.trim());
-                    });
-                });
-
-                UserManagementPage.nextPageButton().then(($btn) => {
-                    if (!$btn.is(':disabled')) {
-                        cy.wrap($btn).click();
-                        cy.wait(300)
-                        checkPage()
-                    } else {
-                        cy.wrap(null).then(() => {
-                            expect(Array.from(seenEmails)).to.have.members(emails);
-                        });
-                    }
-                });
-            }
-            checkPage()
         })
     })
     context('Negative cases', () => {
@@ -234,10 +225,6 @@ describe('User table section', () => {
     beforeEach(() => {
         cy.visit('/')
     })
-    const uniqueName = 'Alicea';
-    const uniqueRole = 'Editor';
-    const uniqueAge = '33';
-    const uniqueEmail = 'alice@site.com.com';
 
     context('Positive cases', () => {
         it('Should check user edit', () => {
@@ -250,19 +237,19 @@ describe('User table section', () => {
             UserManagementPage.emailField().should('have.value', 'alice@site.com')
             UserManagementPage.genderFemaleRadioButton().should('be.checked')
             UserManagementPage.newsletterCheckbox().should('be.checked')
-            UserManagementPage.nameField().clear().type(uniqueName)
-            UserManagementPage.roleField().select(uniqueRole)
-            UserManagementPage.ageField().clear().type(uniqueAge)
-            UserManagementPage.emailField().clear().type(uniqueEmail)
+            UserManagementPage.nameField().clear().type(UserManagementGenerators.validUser.name)
+            UserManagementPage.roleField().select(UserManagementGenerators.validUser.role)
+            UserManagementPage.ageField().clear().type(String(UserManagementGenerators.validUser.age))
+            UserManagementPage.emailField().clear().type(UserManagementGenerators.validUser.email)
             UserManagementPage.genderOtherRadioButton().check()
             UserManagementPage.newsletterCheckbox().uncheck()
             UserManagementPage.productUpdatesCheckbox().check()
             UserManagementPage.addNewUserSaveButton().click()
             UserManagementPage.firstRowInUserTable().within(() => {
-                UserManagementPage.nameDataUserTable().should('contain', uniqueName)
-                UserManagementPage.roleDataUserTable().should('contain', uniqueRole)
-                UserManagementPage.ageDataUserTable().should('contain', uniqueAge)
-                UserManagementPage.emailDataUserTable().should('contain', uniqueEmail)
+                UserManagementPage.nameDataUserTable().should('contain', UserManagementGenerators.validUser.name)
+                UserManagementPage.roleDataUserTable().should('contain', UserManagementGenerators.validUser.role)
+                UserManagementPage.ageDataUserTable().should('contain', UserManagementGenerators.validUser.age)
+                UserManagementPage.emailDataUserTable().should('contain', UserManagementGenerators.validUser.email)
                 UserManagementPage.genderDataUserTable().should('contain', 'Other')
                 UserManagementPage.subscriptionDataUserTable().should('contain', 'Product Updates')
                 UserManagementPage.statusDataUserTable().should('contain', 'Active')
